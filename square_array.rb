2561b74d1f87a9ec65512ef33d38df3ea0067786
Alehandro Persaud <andrew041994@gmail.com>
2020-01-22 17:25:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   # your code here
+  squared_number=[]
   array.each do |num|
-    squared_number = num * num
+    squared_number << num * num
   end
   squared_number
 end
